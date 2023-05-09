1bac0656b793414f2b5113f6e8a2502e8efdd8c2
Roni Akiola <roni.akiola@hotmail.com>
2023-05-09 19:53:33 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import './styles/style.scss';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
