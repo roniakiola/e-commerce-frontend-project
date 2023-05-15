33eebe8fac51482172d2af6255846341340b43d7
Roni Akiola <roni.akiola@hotmail.com>
2023-05-15 12:58:50 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './styles/style.scss';
+import Home from './views/Home';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
