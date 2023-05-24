9c7bc7b9949fd227b9a926ee84b6f2edabb16431
Roni Akiola <roni.akiola@hotmail.com>
2023-05-24 22:07:21 +0300

    starting out navbar, moved paddings out from body, fixed mistake in global setting for bgcolor in style.scss

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,41 @@
-import { AppBar, Toolbar, Typography, styled } from '@mui/material';
-
-const StyledToolbar = styled(Toolbar)({
-  display: 'flex',
-  justfifyContent: 'space-between',
-});
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <AppBar position='fixed' color='inherit'>
-      <StyledToolbar>
-        <Typography variant='h6'>Test</Typography>
-      </StyledToolbar>
-    </AppBar>
+    <nav>
+      <div className='navbar'>
+        <ul className='navbar__menu-left'>
+          <li className='navbar__item'>
+            <Link to='/' className='navbar__link'>
+              Home
+            </Link>
+          </li>
+          <li className='navbar__item'>
+            <Link to='/products' className='navbar__link'>
+              Products
+            </Link>
+          </li>
+          <li className='navbar__item'>
+            <Link to='/profile' className='navbar__link'>
+              Profile
+            </Link>
+          </li>
+        </ul>
+        <ul className='navbar__menu-right'>
+          <li className='navbar__item'>
+            <Link to='/cart' className='navbar__link'>
+              Cart
+            </Link>
+          </li>
+          <li className='navbar__item'>
+            <Link to='/login' className='navbar__link'>
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
